e021f0789c43818d4a26697ba79a0c0c5bdfe662
ほじょ <takumioden@yahoo.co.jp>
2015-02-07 07:44:25 +0900

    色々

--- PacketGame/PacketGame/Assets/UB/script/controller.cs
+++ PacketGame/PacketGame/Assets/UB/script/controller.cs
@@ -10,16 +10,16 @@ public class controller : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKey(KeyCode.UpArrow)){
+				if(Input.GetKey(KeyCode.RightArrow)){
 			transform.position += new Vector3(0,0,0.1f);
 		}
-		if(Input.GetKey(KeyCode.RightArrow)){
+				if(Input.GetKey(KeyCode.DownArrow)){
 			transform.position += new Vector3(0.1f,0,0);
 		}
-		if(Input.GetKey(KeyCode.DownArrow)){
+				if(Input.GetKey(KeyCode.LeftArrow)){
 			transform.position += new Vector3(0,0,-0.1f);
 		}
-		if(Input.GetKey(KeyCode.LeftArrow)){
+				if(Input.GetKey(KeyCode.UpArrow)){
 			transform.position += new Vector3(-0.1f,0,0);
 		}
 	}
--- PacketGame/PacketGame/Assets/UB/script/syncer.cs
+++ PacketGame/PacketGame/Assets/UB/script/syncer.cs
@@ -3,70 +3,80 @@ using System.Collections;
 using WebSocketSharp;
 using LitJson;
 
-public class syncer : MonoBehaviour {
-	// Use this for initialization
-	public GameObject player;
-	public GameObject block;
-	public GameObject[] blocks;
-	
-	private Queue messageQueue;
-	private WebSocket wss;
-	private int num;
-	private byte[] test = new byte[1];
-	
-	void Awake () {
-		messageQueue = Queue.Synchronized(new Queue());
-		wss = new WebSocket("ws://157.7.65.203:3001/");
-		wss.OnOpen += (o, e) => {
-			Debug.Log("Open");
-		};
-		wss.OnMessage += (o, s) => {
-			string e = s.Data;
+public class syncer : MonoBehaviour
+{
+		// Use this for initialization
+		public GameObject player;
+		public GameObject block_prefab;
+		public GameObject blocks;
+		public GameObject blockparent;
+		private Queue messageQueue;
+		private WebSocket wss;
+		private int num;
+		private byte[] test = new byte[1];
+
+		void Awake ()
+		{
+				messageQueue = Queue.Synchronized (new Queue ());
+				wss = new WebSocket ("ws://157.7.65.203:3001/");
+				wss.OnOpen += (o, e) => {
+						Debug.Log ("Open");
+				};
+				wss.OnMessage += (o, s) => {
+						string e = s.Data;
 //			LitJson.JsonData jsonData =  LitJson.JsonMapper.ToObject(e);
-			//Vector3 pos = ToVector3 (jsonData["position"]);
-			messageQueue.Enqueue (e);
-		};
-		wss.Connect ();
-	}
-	
-	void Start(){
-		num = Random.Range (0, 20);
-	}
-	
-	void Update(){
-		lock (messageQueue.SyncRoot) {
-			if (messageQueue.Count > 0) {
-				var message = messageQueue.Dequeue ();
-				Debug.Log (message);
-				LitJson.JsonData jsonData =  LitJson.JsonMapper.ToObject(message.ToString());
-				string type = (string)jsonData["type"];
-				Debug.Log (type);
-				string position_x = (string)jsonData["coordinate_x"];
-				string position_z = (string)jsonData["coordinate_z"];
-
-				float f_position_x = float.Parse (position_x);
-				float f_position_z = float.Parse (position_z);
-
-				Debug.Log (f_position_x);
-				int block_num = 0;
-				if (type == "generate") {
-//					blocks[block_num] = new GameObject("block_" + block_num);
-					blocks[block_num]=(GameObject)Instantiate (block,new Vector3(f_position_x,1,f_position_z),block.transform.rotation) as GameObject;
-					block_num++;
-				}
-			}
-			string jsonText = "{ \"id\" : \"\",  \"position\" : \""+player.transform.position+"\" }";
-//			wss.Send(jsonText);
+						//Vector3 pos = ToVector3 (jsonData["position"]);
+						messageQueue.Enqueue (e);
+				};
+				wss.Connect ();
 		}
-	}
 
-	Vector3 ToVector3(LitJson.JsonData jsonData){
-		string data = (string)jsonData;
-		data = data.Remove (0,1);
-		int length = data.Length;
-		data = data.Remove (length-1, 1);
+		void Start ()
+		{
+				num = Random.Range (0, 20);
+		}
+
+		int block_num = 0;
+
+		void Update ()
+		{
+				lock (messageQueue.SyncRoot) {
+						if (messageQueue.Count > 0) {
+								var message = messageQueue.Dequeue ();
+								Debug.Log (message);
+								LitJson.JsonData jsonData = LitJson.JsonMapper.ToObject (message.ToString ());
+								Debug.Log ((string)jsonData ["type"]);
+								string type = (string)jsonData ["type"];
+								Debug.Log (type);
+
+								if (type == "generate") {
+										string position_x = (string)jsonData ["coordinate_x"];
+										string position_z = (string)jsonData ["coordinate_z"];
+
+										float f_position_x = float.Parse (position_x);
+										float f_position_z = float.Parse (position_z);
+										blocks = (GameObject)Instantiate (block_prefab, new Vector3 (f_position_z, 1, f_position_x), block_prefab.transform.rotation) as GameObject;
+										blocks.transform.name = "block_" + block_num;
+										blocks.transform.parent = blockparent.transform;
+										Debug.Log (blocks);
+										block_num++;
+
+								}
+						}
+						string jsonText = "{ \"type\" : \"position\",  \"position\" : \"" + player.transform.position.x + ","+ player.transform.position.z + "\" }";
+						wss.Send(jsonText);
+//						Debug.Log (jsonText);
+				}
+		}
+
+		Vector3 ToVector3 (LitJson.JsonData jsonData)
+		{
+				string data = (string)jsonData;
+				data = data.Remove (0, 1);
+				int length = data.Length;
+				data = data.Remove (length - 1, 1);
 		
-		string[] array = data.Split(',');
-		return new Vector3(float.Parse(array[0]), float.Parse(array[1]), float.Parse(array[2]));
-	}
+				string[] array = data.Split (',');
+				return new Vector3 (float.Parse (array [0]), float.Parse (array [1]), float.Parse (array [2]));
+		}
 }
